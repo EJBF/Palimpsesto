1507a368b05ed60b55ef4098f1b0cac7ee91a10c
EJBF <110767901+EJBF@users.noreply.github.com>
2022-08-07 12:30:27 +0100

    Add files via upload

---
+++ frontend/build/precache-manifest.bcdd03b0b90b860bd447b453024123fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b21189e34a288b379628dd8dfde5cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9262598054f54d5ee75e",
+    "url": "/static/css/main.19a9c7e3.chunk.css"
+  },
+  {
+    "revision": "ddd2516606fb8d89433e",
+    "url": "/static/js/2.085612ba.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.085612ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9262598054f54d5ee75e",
+    "url": "/static/js/main.022e395b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
